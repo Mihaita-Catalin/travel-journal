b254f69bcae16d2a8a03d6d02d1a5c3da0b41380
Mihaita-Catalin Pavel <pavel.mihaita.catalin2000@gmail.com>
2021-02-20 18:50:00 +0200

    added navigation drawer and set up the splash screen

---
+++ app/src/main/java/com/example/traveljournal/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.traveljournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_TO_BE_DISPLAYED = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_TO_BE_DISPLAYED);
+    }
+}
